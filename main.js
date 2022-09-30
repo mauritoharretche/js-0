98b7e30a053712e63fb4633b90e2ac67ab12fe41
maurito.harretche <mauroharretche@gmail.com>
2022-09-30 10:15:56 -0300

    avance

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-let miNombre;
+/* let miNombre;
 
 miNombre = "Mauro";
 
@@ -62,16 +62,118 @@ console.log(miNombre);
 console.log(miApellido);
 console.log(miEdad);
 console.log(miMascota);
-console.log(edadMascota);
+console.log(edadMascota); */
+
+
+const alumno = {
+    nombre : 'mauro',
+    edad : 70,
+    pelo: 'castaño',
+    altura: 1.73,
+    peso: 66
+}
+
+console.table(alumno)
+
+
+const mascota = {
+    nombre : 'polly',
+    edad : 4,
+    pelo: 'rubio',
+    altura: 45,
+    peso: 7
+}
+
+console.table(mascota)
+
+let frutas = [ "banana", "pera" , "frutilla" , "naranja" ,"mango"]
+   /*  {
+        tipo : "banana",
+        color : "amarillo"
+    },
+    {
+        tipo : "naranja",
+        color : "naranja"
+    },
+    {
+        tipo : "frutilla",
+        color : "rojo"
+    },
+    {
+        tipo: "anana",
+        color : "marron"
+    },
+    {
+        tipo : "mango",
+        color : "anaranjado"
+    } */
+
+
+console.log(frutas)
+
+console.log( frutas[0])
+console.log( frutas[1])
+console.log( frutas[2])
+console.log( frutas[3])
+console.log( frutas[4])
 
 
 
+let numeros = [ 0 , 1 , 2 , 3, 4]
+    /* {
+        numero : 0,
+    },
+    {
+        numero : 1,
+    },
+    {
+        numero : 2,
+    },
+    {
+        numero : 3,
+    },
+    {
+        numero : 4,
+    } */
+
+
+console.log(numeros)
+
+console.log(numeros[0])
+console.log(numeros[1])
+console.log(numeros[2])
+console.log(numeros[3])
+console.log(numeros[4])
+
+let familia = [ "nico" , "jose" , "pablo" , "mariel" , "mauro"]
+    /* {
+        nombre : 'nico',
+    },
+    {
+        nombre : 'jose',
+    },
+    {
+        nombre : 'pablo',
+    },
+    {
+        nombre : 'mariel',
+    },
+    {
+        nombre : 'mauro',
+    } */
+
+console.log(familia)
+
+console.log(familia[0])
+console.log(familia[1])
+console.log(familia[2])
+console.log(familia[3])
+console.log(familia[4])
 
 
 
+/* Crear una variable llamada textoAleatorio formando una frase con el segundo elemento del array del punto 13, el cuarto elemento del punto 14 y el quinto objeto del array del punto 15. */
 
+let textoAleatorio = "hola como muchas" + " " + frutas[2] + "una cantidad de" +  " " + numeros[3] + familia[4]
 
-
-
-
-
+console.log(textoAleatorio)
