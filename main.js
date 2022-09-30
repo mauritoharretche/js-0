e730e8d978285396f3a21ed276b4c4d86f5ff948
maurito.harretche <mauroharretche@gmail.com>
2022-09-30 00:27:08 -0300

    alpha

---
+++ main.js
@@ -0,0 +1,75 @@
+let miNombre;
+
+miNombre = "Mauro";
+
+console.log(miNombre);
+
+let miApellido;
+
+miApellido = "Harretche";
+
+console.log(miApellido);
+
+let miEdad;
+
+miEdad = 30;
+
+console.log(miEdad);
+
+let miMascota;
+
+miMascota = "Polly";
+
+console.log(miMascota);
+
+let edadMascota;
+
+edadMascota = 4;
+
+console.log(edadMascota);
+
+let nombreCompleto = miNombre + " " + miApellido
+
+console.log(nombreCompleto);
+
+let textoPresentacion = "Mi nombre es " + miNombre + " " + miApellido;
+
+console.log(textoPresentacion);
+
+let sumaEdades = miEdad + edadMascota;
+
+console.log(sumaEdades);
+
+let restaEdades = miEdad - edadMascota;
+
+console.log(restaEdades);
+
+let productoEdades = miEdad * edadMascota;
+
+console.log(productoEdades);
+
+let divisionEdades = miEdad / edadMascota;
+
+console.log(divisionEdades); 
+
+miNombre = prompt(" Ingrese su nombre")
+miApellido = prompt("Ingrese su apellido")
+miEdad = prompt("Ingrese su edad")
+miMascota = prompt("Ingrese nombre de su mascota")
+edadMascota = prompt("Ingrese la edad de su mascota")
+
+console.log(miNombre);
+console.log(miApellido);
+console.log(miEdad);
+console.log(miMascota);
+console.log(edadMascota);
+
+
+
+
+
+
+
+
+
+
